7e2e0bf55e40190126c9a20de41cb10c67fdcee1
ABHILASH <abhilash.barbade@gmail.com>
2019-05-26 16:58:37 +0530

    image save

--- webscraper.py
+++ webscraper.py
@@ -2,15 +2,17 @@ import requests
 import bs4
 import shutil
 import xlwt
-from django.http import HttpResponse
+import os
+import datetime
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__).decode('utf-8'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 SCRAP_MAIN_URL = 'http://www.agriculture.gov.au/pests-diseases-weeds/plant#identify-pests-diseases'
-
 BASE_URL ="http://www.agriculture.gov.au"
 
 def scrap_data():
-        link_list = []
-
         scraped_data  = []
         res = requests.get(SCRAP_MAIN_URL)
 
@@ -18,10 +20,10 @@ def scrap_data():
         images = bs4.BeautifulSoup(res.text,'html').find('ul', class_='flex-container').find_all("li")
         for ul in images[16:]:
             data_list = []
-            a = ul.find('a')
+            href = ul.find('a')
             count+=1
             print(count)
-            scrap_url = (a['href'])
+            scrap_url = (href['href'])
             try:
                 if scrap_url:
 
@@ -38,26 +40,38 @@ def scrap_data():
                     image_src= soup.find('div', {"class": "pest-header-image"})
 
                     if image_src :
-                        data_list.append(get_img_src_and_save(image_src))
+                        image_url = get_img_src_and_save(image_src)
+                        if not str(image_url) == "Not Found":
+                            data_list.append(image_url)#get image path
+                            data_list.append(save_image(image_url))#save image and get append imafe path
+                        else:
+                            data_list.append(image_url)
+                            data_list.append("Not Found")
 
                     elif soup.find('div', {"class": "alignnone"}):
-                        data_list.append(get_img_src_and_save(soup.find('div', {"class": "alignnone"})))
+                        image_url = get_img_src_and_save(soup.find('div', {"class": "alignnone"}))
+                        if not str(image_url) == "Not Found":
+                            data_list.append(image_url)  # get image path
+                            data_list.append(save_image(image_url))  # save image and get append imafe path
+                        else:
+                            data_list.append(image_url)
+                            data_list.append("Not Found")
 
                     else:
                         data_list.append("Not Found")
 
-                    origin= soup.find('div', {"class": "pest-header-content"})
+                    origin= soup.find('div', {"class": "pest-header-content"})#finding origin parent div
 
                     if origin and origin.find("strong",text='Origin: '):
-                        data_list.append(str(origin.find("strong",text='Origin: ').next_sibling))
+                        data_list.append(str(origin.find("strong",text='Origin: ').next_sibling))#getting origin text
 
                     else:
                         data_list.append("Not Found")
 
-                    pathway = soup.find('div', {"class": "pest-header-content"})
+                    pathway = soup.find('div', {"class": "pest-header-content"})#finding pathway parent div
 
                     if pathway and pathway.find("strong",text='Pathways: '):
-                        data_list.append(str(pathway.find("strong",text='Pathways: ').next_sibling))
+                        data_list.append(str(pathway.find("strong",text='Pathways: ').next_sibling))#finding pathway text
                     else:
                         data_list.append("Not Found")
 
@@ -69,7 +83,7 @@ def scrap_data():
                 break
 
         print("GOING TO END")
-        return  export_data_xls(['Image', 'Origin', 'Pathways'],scraped_data)
+        return  export_data_xls(['Image','Local image path', 'Origin', 'Pathways'],scraped_data)
 
 
 
@@ -93,20 +107,27 @@ def export_data_xls(columns, rows):
 
 def get_img_src_and_save(image_src):
 
-     url = str(image_src.img.get('src'))
-     if url:
-         if  url[0:4] =='http':
+    url = str(image_src.img.get('src'))
+    if url:
+        if  url[0:4] =='http':
             image_url = str(url)
-         else:
+        else:
             image_url = str(BASE_URL + url)
 
-            # response = requests.get(image_url, stream=True)
-            # with open('img.png', 'wb') as out_file:
-            #     shutil.copyfileobj(response.raw, out_file)
+        return image_url
+    else:
+     image_url = "Not Found"
 
-            return image_url
-     else:
-         image_url = "Not Found"
-         return image_url
+     return image_url
+
+
+def save_image(image_url):
+    dirname = datetime.datetime.now().strftime('%Y.%m.%d.%H.%M.%S')
+    filename = "%s_%s.%s" % ("image", dirname, 'png')
+    raw_file_path_and_name = os.path.join(filename)
+    response = requests.get(image_url, stream=True)
+    with open(MEDIA_ROOT + raw_file_path_and_name, 'wb') as out_file:
+        shutil.copyfileobj(response.raw, out_file)
+    return str(out_file.name)
 
 
